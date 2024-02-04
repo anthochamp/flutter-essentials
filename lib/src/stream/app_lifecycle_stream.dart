3c893f130d8e971344a88cc397e373ec69c698f0
Anthony Champagne <dev@anthonychampagne.fr>
2024-02-04 23:02:36 +0100

    replace file header license to spdx

--- lib/src/stream/app_lifecycle_stream.dart
+++ lib/src/stream/app_lifecycle_stream.dart
@@ -1,6 +1,6 @@
-// Copyright 2023, Anthony Champagne. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// SPDX-FileCopyrightText: © 2023 - 2024 Anthony Champagne <dev@anthonychampagne.fr>
+//
+// SPDX-License-Identifier: BSD-3-Clause
 
 import 'dart:async';
 
